8e82a8ae73c238981b3ca639d4d746d17e0e232e
CasteAwayy <140751958+CasteAwayy@users.noreply.github.com>
2024-12-16 17:44:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-//your JS code here. If required.
+const form = document.querySelector(".form");
+
+const wait = function (age) {
+  return new Promise(function (resolve, reject) {
+    if (age > 18) setTimeout(resolve, 4000);
+    setTimeout(reject, 4000);
+  });
+};
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const formData = new FormData(e.target);
+  const age = formData.get("age");
+  const name = formData.get("name");
+  wait(age)
+    .then(() => {
+      alert(`Welcome, ${name}. You can vote.`);
+    })
+    .catch(() => {
+      alert(`Oh sorry ${name}. You aren't old enough.`);
+    });
+});
